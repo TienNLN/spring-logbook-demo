91e033a2d6b936a9b676d7db1202559cb54270f4
Nguyen Lam Nhat Tien <tien20001810@gmail.com>
2023-01-12 10:17:05 +0700

    add interceptor to resttemplate & upgrade to boot 2.7.7

--- src/main/java/com/tiennln/springlogbookokhttpdemo/configs/RestTemplateConfig.java
+++ src/main/java/com/tiennln/springlogbookokhttpdemo/configs/RestTemplateConfig.java
@@ -1,9 +1,16 @@
 package com.tiennln.springlogbookokhttpdemo.configs;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.httpclient.LogbookHttpRequestInterceptor;
+import org.zalando.logbook.httpclient.LogbookHttpResponseInterceptor;
 
 /**
  * @author TienNLN on 11/01/2023
@@ -11,9 +18,21 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Configuration
 public class RestTemplateConfig {
 
+    @Autowired
+    private Logbook logbook;
+
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
+
+        CloseableHttpClient closeableHttpClient = HttpClientBuilder.create()
+                .addInterceptorFirst(new LogbookHttpRequestInterceptor(logbook))
+                .addInterceptorLast(new LogbookHttpResponseInterceptor())
+                .build();
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory(closeableHttpClient);
+
+        restTemplate.setRequestFactory(httpRequestFactory);
+
         return restTemplate;
     }
 }
